1ca71b3b853470e594b4d9fab69e4218a2ce355c
apple72214-beep <apple72214@gmail.com>
2026-09-14 02:06:51 +0000

    feat(web): v0.3a bilingual AR/EN report draft + radiologist sign-off

--- web/sw.js
+++ web/sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for the browser edition (GitHub Pages). */
-const CACHE = "rrai-web-v2";
-const SHELL = ["./", "./index.html", "./app.js", "./dicom.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "rrai-web-v3";
+const SHELL = ["./", "./index.html", "./app.js", "./dicom.js", "./report.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
