ad0b2b4b8332d82fc32e8695b025f8f2c7ca27b3
apple72214-beep <apple72214@gmail.com>
2026-09-15 23:33:00 +0000

    feat(web): v0.5.1 context-aware triage (brain/chest/bone) + reasons + brain samples + permanent tests

--- web/sw.v16.js
+++ web/sw.v16.js
@@ -1,12 +1,12 @@
-const BUILD = "v18";
-const CACHE = "rrai-web-v18";
+const BUILD = "v16";
+const CACHE = "rrai-web-v16";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v18.html", "./app.v18.js", "./dicom.v18.js", "./report.v18.js", "./sw.v18.js"
+  "./ui.v16.html", "./app.v16.js", "./dicom.v16.js", "./report.v16.js", "./sw.v16.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v18.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v16.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
--- web/sw.v17.js
+++ web/sw.v17.js
@@ -1,12 +1,12 @@
-const BUILD = "v18";
-const CACHE = "rrai-web-v18";
+const BUILD = "v17";
+const CACHE = "rrai-web-v17";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v18.html", "./app.v18.js", "./dicom.v18.js", "./report.v18.js", "./sw.v18.js"
+  "./ui.v17.html", "./app.v17.js", "./dicom.v17.js", "./report.v17.js", "./sw.v17.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v18.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v17.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
