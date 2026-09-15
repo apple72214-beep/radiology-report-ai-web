a0a15eb6235aa66316de6c4d17c0f2e6630b1a4a
apple72214-beep <apple72214@gmail.com>
2026-09-15 21:59:08 +0000

    feat(web): v16 viewer interaction fix + docs: v0.4 OFFICIALLY CLOSED (on-device render victory)

--- web/sw.v14.js
+++ web/sw.v14.js
@@ -1,12 +1,12 @@
-const BUILD = "v16";
-const CACHE = "rrai-web-v16";
+const BUILD = "v14";
+const CACHE = "rrai-web-v14";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v16.html", "./app.v16.js", "./dicom.v16.js", "./report.v16.js", "./sw.v16.js"
+  "./ui.v14.html", "./app.v14.js", "./dicom.v14.js", "./report.v14.js", "./sw.v14.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v16.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v14.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
--- web/sw.v15.js
+++ web/sw.v15.js
@@ -1,12 +1,12 @@
-const BUILD = "v16";
-const CACHE = "rrai-web-v16";
+const BUILD = "v15";
+const CACHE = "rrai-web-v15";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v16.html", "./app.v16.js", "./dicom.v16.js", "./report.v16.js", "./sw.v16.js"
+  "./ui.v15.html", "./app.v15.js", "./dicom.v15.js", "./report.v15.js", "./sw.v15.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v16.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v15.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
