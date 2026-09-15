08a1914b5d7bb869adeb101ef1d6ffc63fd9ea23
apple72214-beep <apple72214@gmail.com>
2026-09-15 22:05:17 +0000

    feat(web): v17 fit-to-width viewer + centering (layout-race fix)

--- web/sw.v15.js
+++ web/sw.v15.js
@@ -1,12 +1,12 @@
-const BUILD = "v17";
-const CACHE = "rrai-web-v17";
+const BUILD = "v15";
+const CACHE = "rrai-web-v15";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v17.html", "./app.v17.js", "./dicom.v17.js", "./report.v17.js", "./sw.v17.js"
+  "./ui.v15.html", "./app.v15.js", "./dicom.v15.js", "./report.v15.js", "./sw.v15.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v17.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v15.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
