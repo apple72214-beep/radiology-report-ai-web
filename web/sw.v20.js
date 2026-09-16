769f9ae48488a3f7b4dd7d1d525aa6972bee90fe
apple72214-beep <apple72214@gmail.com>
2026-09-16 22:25:59 +0000

    v22 full tree (web v14-v22 + docker + docs screenshots)

--- web/sw.v20.js
+++ web/sw.v20.js
@@ -1,12 +1,12 @@
-const BUILD = "v22";
-const CACHE = "rrai-web-v22";
+const BUILD = "v20";
+const CACHE = "rrai-web-v20";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v22.html", "./app.v22.js", "./dicom.v22.js", "./report.v22.js", "./sw.v22.js"
+  "./ui.v20.html", "./app.v20.js", "./dicom.v20.js", "./report.v20.js", "./sw.v20.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v22.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v20.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
--- web/sw.v21.js
+++ web/sw.v21.js
@@ -1,12 +1,12 @@
-const BUILD = "v22";
-const CACHE = "rrai-web-v22";
+const BUILD = "v21";
+const CACHE = "rrai-web-v21";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v22.html", "./app.v22.js", "./dicom.v22.js", "./report.v22.js", "./sw.v22.js"
+  "./ui.v21.html", "./app.v21.js", "./dicom.v21.js", "./report.v21.js", "./sw.v21.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v22.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v21.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
