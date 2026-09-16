7b226ab6781b53dd8ca2d9202e36c092a8dde3c6
a <a@b>
2026-09-16 21:51:43 +0000

    merge private (ours)

--- web/sw.v15.js
+++ web/sw.v15.js
@@ -1,12 +1,12 @@
-const BUILD = "v20";
-const CACHE = "rrai-web-v20";
+const BUILD = "v15";
+const CACHE = "rrai-web-v15";
 const CRITICAL = [
   "./", "./start.html", "./index.html", "./manifest.webmanifest",
-  "./ui.v20.html", "./app.v20.js", "./dicom.v20.js", "./report.v20.js", "./sw.v20.js"
+  "./ui.v15.html", "./app.v15.js", "./dicom.v15.js", "./report.v15.js", "./sw.v15.js"
 ];
 const BEST_EFFORT = [
   "./icons/icon-192.png", "./icons/icon-512.png",
-  "./codecs/decode.v20.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
+  "./codecs/decode.v15.js", "./codecs/charlswasm.js", "./codecs/charlswasm.wasm",
   "./codecs/openjpegwasm.js", "./codecs/openjpegwasm.wasm", "./codecs/openjphjs.js", "./codecs/openjphjs.wasm"
 ];
 self.addEventListener("install", (e) => {
