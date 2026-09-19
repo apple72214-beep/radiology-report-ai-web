4bbb57398fc9f3258cb044fd0ae80c104d928b00
apple72214-beep <apple72214@gmail.com>
2026-09-19 22:43:06 +0000

    v43 sync: whole-folder DICOM picker + ZIP-magic ingest, bootstrap b2.15, sw v43

--- tests/docx_test.mjs
+++ tests/docx_test.mjs
@@ -1,5 +1,5 @@
-import { buildDocxReport, buildDocxReportEn, crc32 } from "../docx.v42.js";
-import { signedDocHash } from "../report.v42.js";
+import { buildDocxReport, buildDocxReportEn, crc32 } from "../docx.v43.js";
+import { signedDocHash } from "../report.v43.js";
 import fs from "node:fs";
 let fails = 0;
 const enc = new TextEncoder();
--- tests/pacs_test.mjs
+++ tests/pacs_test.mjs
@@ -1,4 +1,4 @@
-import { parseMultipartRelated } from "../dicom.v42.js";
+import { parseMultipartRelated } from "../dicom.v43.js";
 let fails = 0;
 const enc = new TextEncoder();
 const bnd = "abc123";
