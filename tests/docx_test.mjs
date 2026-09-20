061df8b507bf47cf18a0abdcc149ccc83aacc2df
apple72214-beep <apple72214@gmail.com>
2026-09-20 00:02:01 +0000

    v45 sync: banner-constant fix, bootstrap b2.17, sw v45

--- tests/docx_test.mjs
+++ tests/docx_test.mjs
@@ -1,5 +1,5 @@
-import { buildDocxReport, buildDocxReportEn, crc32 } from "../docx.v44.js";
-import { signedDocHash } from "../report.v44.js";
+import { buildDocxReport, buildDocxReportEn, crc32 } from "../docx.v45.js";
+import { signedDocHash } from "../report.v45.js";
 import fs from "node:fs";
 let fails = 0;
 const enc = new TextEncoder();
